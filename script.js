d55da140b5a92079b0eaf597a892e8e1709a9103
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-06-18 18:45:46 -1000

    hourly color changee added

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-function dailySched(){
-    const timeArray = ["9AM", "10AM", "11AM","12"]
-    
-    let html = "";
 
-    timeArray.forEach(time => {
+var currentDate = $("#currentDay");
+currentDate.text(moment().format('MMMM Do YYYY, h:mm a'))
+
+var currentTime = moment().format('H');
+
+
+
+const timeArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+
+
+function dailySched() {
+
+    let html = "";  
+    
+    timeArray.forEach( (time, i)=> {
+        const hr = i+9;
+        const nw = moment().hour();
+        const color = (hr === nw) ? "present" :
+        (hr > nw) ? "future" :
+        "past";
+        
         html += `<div class="row">
         <div class="col-md-1 hour d-flex justify-content-center align-items-center">${time}</div> 
-        <input class='user-event-input col-md-10 event-input-block' id="${time}Row"> 
-        <div class="col-md-1 saveBtn" data-hour=9><i class="fas fa-save pl-4 pt-4" aria-hidden="true"></i></div> 
+        <input class='user-event-input col-md-10 event-input-block ${color}' id="${time}Row"> 
+        <div class="col-md-1 saveBtn" data-hour=${hr}><i class="fas fa-save pl-4 pt-4 " aria-hidden="true"></i></div> 
     </div>`
 
     });
 
-    $("#mainContainer").append(html)
+    $("#mainContainer").append(html);
+
+
 }
 dailySched();
+
+
+
+
+
+
+// ===== Looping through 9am to 17pm ===== //
+for (i = 9; i < 18; i++) {
+    // Grab value from local storage.
+    var inputValue = localStorage.getItem('time' + i);
+    if (!inputValue) {
+        inputValue = '';
+    }
+}
+
